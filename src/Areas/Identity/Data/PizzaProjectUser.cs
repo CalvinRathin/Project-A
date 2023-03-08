b4c741272e8e176a272912232ddb7adc8e1448fb
CalvinRathin <113708133+CalvinRathin@users.noreply.github.com>
2023-03-08 17:29:03 +0530

    Add files via upload

---
+++ src/Areas/Identity/Data/PizzaProjectUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace PizzaProject.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the PizzaProjectUser class
+public class PizzaProjectUser : IdentityUser
+{
+}
+
